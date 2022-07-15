31f500581b115a7a6a33203e2685fd2477f8e987
asunawesker <zs19004913@estudiantes.uv.mx>
2022-07-14 21:32:01 -0500

    SUbiendo actividad 10 dsr

---
+++ src/java/org/sample/whiteboardapp/MyWhiteboard.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.sample.whiteboardapp;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+/**
+ *
+ * @author asunawesker
+ */
+@ServerEndpoint("/whiteboardendpoint")
+public class MyWhiteboard {
+    private static Set<Session> peers = Collections.synchronizedSet(new HashSet<Session>());
+
+    @OnMessage
+    public String onMessage(String message) {
+        return null;
+    }
+    
+    @OnOpen
+    public void onOpen (Session peer) {
+        peers.add(peer);
+    }
+
+    @OnClose
+    public void onClose (Session peer) {
+        peers.remove(peer);
+    }
+
+}
